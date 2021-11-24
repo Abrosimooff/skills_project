98b44b28462ecdc1aaea28170ec9b34fdadb47c9
victor <abrosimov@vistgroup.ru>
2021-11-24 15:47:40 +0700

    fiz

--- advent_calendar_app/urls.py
+++ advent_calendar_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from advent_calendar_app.views import MRCSkillAdventCalendarView, CalendarTaskView
-from santa_app.views import *
 
 urlpatterns = [
     path('', MRCSkillAdventCalendarView.as_view(), name='mrc-skills-advent-calendar'),
